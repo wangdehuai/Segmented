c3b45bc3f0db25b523123ee48b9aac8c932ee97d
wangdehuai <wangdehuai@me.com>
2014-12-12 16:53:33 +0800

    root

---
+++ Segmented/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Segmented
+//
+//  Created by wangdehuai on 14/12/12.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Segmented/RootViewController.m
@@ -0,0 +1,25 @@
+//
+//  RootViewController.m
+//  Segmented
+//
+//  Created by wangdehuai on 14/12/12.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
