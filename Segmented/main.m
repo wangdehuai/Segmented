88673b40520230570e37c68353ff1466f7fbd233
wangdehuai <wangdehuai@me.com>
2014-12-12 16:47:58 +0800

    Initial Commit

---
+++ Segmented/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Segmented
+//
+//  Created by wangdehuai on 14/12/12.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
